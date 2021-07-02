232883b1a83465f4c4736e348330069ac74969f2
legitatx <ryan.donaldson@simplify.jobs>
2021-07-02 00:29:56 -0400

    ✅ Update legacy tests
    We will always use webpack 5+, remove version check for stub to avoid crazy workaround hack

--- specs/ExtensionReloader.specs.ts
+++ specs/ExtensionReloader.specs.ts
@@ -1,5 +1,5 @@
 import { assert } from "chai";
-import { SinonStub, spy, stub } from "sinon";
+import { stub } from "sinon";
 import * as webpack from "webpack";
 import ExtensionReloaderImpl from "../src/ExtensionReloader";
 import { IExtensionReloaderInstance } from "../typings/webpack-extension-reloader";
@@ -11,30 +11,28 @@ describe("ExtensionReloader", () => {
     ExtensionReloaderImpl.prototype,
     "_registerPlugin",
   ).returns();
-  const versionCheckSpy = spy(ExtensionReloaderImpl.prototype._isWebpackGToEV4);
 
-  function pluginFactory(
-    version: string,
-  ): [IExtensionReloaderInstance, SinonStub] {
-    const webpackStub = stub(webpack, "version").value(version);
+  function pluginFactory(): IExtensionReloaderInstance {
     const plugin = new ExtensionReloaderImpl();
-    return [plugin, webpackStub];
+    return plugin;
   }
 
   beforeEach(() => {
     registerStub.reset();
-    versionCheckSpy.resetHistory();
     process.env = { ...envCopy };
   });
 
   describe("When applying plugin, should check if is in development mode", () => {
     it("Should check for --mode flag on versions >= 4", () => {
-      const [plugin, stubbed] = pluginFactory("4.2.21");
-      const mockedCompiler = { options: { mode: "development" } } as webpack.Compiler;
-      plugin.apply(mockedCompiler);
-      assert(registerStub.called);
+      const plugin = pluginFactory();
+      const mockedCompiler = { options: {} } as webpack.Compiler;
 
-      stubbed.restore();
+      plugin.apply(mockedCompiler);
+      assert(registerStub.notCalled);
+
+      mockedCompiler.options.mode = "development";
+      plugin.apply(mockedCompiler);
+      assert(registerStub.calledOnce);
     });
   });
 });
