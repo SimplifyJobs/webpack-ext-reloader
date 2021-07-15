71b2c672961c4eea60f3df3b9dc7009e691e9cd1
legitatx <ryan.donaldson@simplify.jobs>
2021-07-15 14:54:45 -0400

    🐛 Fix chunk optimization

--- src/webpack/CompilerEventsFacade.ts
+++ src/webpack/CompilerEventsFacade.ts
@@ -18,12 +18,11 @@ export default class CompilerEventsFacade {
         const afterOptimizeChunkAssets = (chunks) => {
           call(comp, chunks);
         };
-        /* https://github.com/webpack/webpack/blob/main/lib/Compilation.js#L772-L779
-        afterOptimizeChunkAssets = PROCESS_ASSETS_STAGE_OPTIMIZE + 1
-        */
-        const stage = Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE + 1;
         (comp.hooks as any).processAssets.tap(
-          { name: CompilerEventsFacade.extensionName, stage },
+          {
+            name: CompilerEventsFacade.extensionName,
+            stage: Compilation.PROCESS_ASSETS_STAGE_ANALYSE,
+          },
           afterOptimizeChunkAssets,
         );
       },
