a7da40fd3304933e173d4bc326ebd49aad56c92e
legitatx <ryan.donaldson@simplify.jobs>
2021-08-05 10:41:39 -0400

    🔨 Fix dev sample script

--- sample/webpack.plugin.js
+++ sample/webpack.plugin.js
@@ -10,13 +10,13 @@ module.exports = {
   entry: {
     "content-script": "./sample/plugin-src/my-content-script.js",
     background: "./sample/plugin-src/my-background.js",
-    popup: "./sample/plugin-src/popup.js"
+    popup: "./sample/plugin-src/popup.js",
   },
   output: {
     publicPath: ".",
     path: resolve(__dirname, "dist/"),
     filename: "[name].bundle.js",
-    libraryTarget: "umd"
+    libraryTarget: "umd",
   },
   plugins: [
     /***********************************************************************/
@@ -26,28 +26,30 @@ module.exports = {
       entries: {
         contentScript: "content-script",
         background: "background",
-        extensionPage: "popup"
-      }
+        extensionPage: "popup",
+      },
       // Also possible to use
       // manifest: resolve(__dirname, "manifest.json")
     }),
 
     new MiniCssExtractPlugin({ filename: "style.css" }),
-    new CopyWebpackPlugin([
-      {
-        /***********************************************************************/
-        /* If you have different configurations for development and production,*/
-        /* you can have two manifests (one for each environment)               */
-        /***********************************************************************/
-        from:
-          process.env.NODE_ENV === "development"
-            ? "./sample/manifest.dev.json"
-            : "./sample/manifest.prod.json",
-        to: "manifest.json"
-      },
-      { from: "./sample/plugin-src/popup.html" },
-      { from: "./sample/icons" }
-    ])
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          /***********************************************************************/
+          /* If you have different configurations for development and production,*/
+          /* you can have two manifests (one for each environment)               */
+          /***********************************************************************/
+          from:
+            process.env.NODE_ENV === "development"
+              ? "./sample/manifest.dev.json"
+              : "./sample/manifest.prod.json",
+          to: "manifest.json",
+        },
+        { from: "./sample/plugin-src/popup.html" },
+        { from: "./sample/icons" },
+      ],
+    }),
   ],
   module: {
     rules: [
@@ -57,23 +59,23 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: [require("@babel/preset-env")]
-          }
-        }
+            presets: [require("@babel/preset-env")],
+          },
+        },
       },
       {
         test: /\.css$/,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader
+            loader: MiniCssExtractPlugin.loader,
           },
-          "css-loader"
-        ]
+          "css-loader",
+        ],
       },
       {
         test: /\.txt$/,
-        use: "raw-loader"
-      }
-    ]
-  }
+        use: "raw-loader",
+      },
+    ],
+  },
 };
