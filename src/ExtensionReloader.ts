d15c044f596114d7f95d4e22e327be53ad0ff6f5
Alec WM <alec@owls.io>
2022-05-10 19:09:58 +1000

    Start server only after the first emit (#268)
    Problem:
    The server is started before webpack emits the initial changes and
    built files are written to disk, causing the installed extension,
    which is trying to reconnect, to reload without having files to load.
    This caused an error in the extension, where it needs to be removed
    and installed again.
    
    Solution:
    We start the server after the first webpack emit event
    
    Fixes #51
    
    Co-authored-by: Francisco Lourenco <aristidesfl@gmail.com>

--- src/ExtensionReloader.ts
+++ src/ExtensionReloader.ts
@@ -87,7 +87,6 @@ export default class ExtensionReloaderImpl extends AbstractPluginReloader implem
 
     this._eventAPI = new CompilerEventsFacade(compiler);
     this._injector = middlewareInjector(parsedEntries, { port, reloadPage });
-    this._triggerer = changesTriggerer(port, reloadPage);
     this._eventAPI.afterOptimizeChunks((comp, chunks) => {
       comp.assets = {
         ...comp.assets,
@@ -96,6 +95,9 @@ export default class ExtensionReloaderImpl extends AbstractPluginReloader implem
     });
 
     this._eventAPI.afterEmit((comp) => {
+      // reload page after first emit
+      if (!this._triggerer) this._triggerer = changesTriggerer(port, reloadPage);
+
       const { contentOrBgChanged, onlyPageChanged } = this._whatChanged(comp.chunks, parsedEntries);
 
       if (contentOrBgChanged || onlyPageChanged) {
