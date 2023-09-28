0c4fe869f009788fc8bd2f272cf749c22b6a3247
vialoh <57686023+vialoh@users.noreply.github.com>
2023-09-28 23:19:40 +0200

    ✨ MV3 + other misc improvements/fixes (#386)
    * chore: lint
    
    * add compatibility with manifest V3
    
    * :bug: Use `cross-env` for all scripts
    
    * :bug: Use JSON5 to parse manifest.json (closes #216)
    
    * :bug: Fix type declarations (closes #209)
    
    * :memo: Updated README
    
    ---------
    
    Co-authored-by: Louis-Marie Michelin <lmichelin@outlook.fr>
    Co-authored-by: Luke Hager <luke@fullstacked.services>
    Co-authored-by: Rushil Srivastava <11495473+rushilsrivastava@users.noreply.github.com>

--- src/utils/manifest.ts
+++ src/utils/manifest.ts
@@ -1,5 +1,6 @@
 import { readFileSync } from "fs";
 import { flatMapDeep } from "lodash";
+import JSON5 from "json5";
 import { Compiler, Entry } from "webpack";
 import { bgScriptEntryErrorMsg, bgScriptManifestRequiredMsg } from "../messages/errors";
 
@@ -8,19 +9,19 @@ export function extractEntries(
   manifestPath: string,
   webpackOutput: Compiler["options"]["output"] = {},
 ): IEntriesOption {
-  const manifestJson = JSON.parse(readFileSync(manifestPath).toString()) as IExtensionManifest;
-  const { background, content_scripts } = manifestJson;
+  const manifestJson = JSON5.parse(readFileSync(manifestPath).toString()) as IExtensionManifest;
+  const { background, content_scripts: contentScripts } = manifestJson;
   const { filename } = webpackOutput;
 
   if (!filename) {
     throw new Error("Please specify the `output.filename` in your webpack config.");
   }
 
-  if (!background?.scripts) {
+  if (!(background?.scripts || background?.service_worker)) {
     throw new TypeError(bgScriptManifestRequiredMsg.get());
   }
 
-  const bgScriptFileNames = background.scripts;
+  const bgScriptFileNames = background.service_worker ? [background.service_worker] : background.scripts ?? [];
   const toRemove = (filename as string).replace("[name]", "");
 
   const bgWebpackEntry = Object.keys(webpackEntry).find((entryName) =>
@@ -31,9 +32,9 @@ export function extractEntries(
     throw new TypeError(bgScriptEntryErrorMsg.get());
   }
 
-  const contentEntries: unknown = content_scripts
+  const contentEntries: unknown = contentScripts
     ? flatMapDeep(Object.keys(webpackEntry), (entryName) =>
-        content_scripts.map(({ js }) =>
+        contentScripts.map(({ js }) =>
           js.map((contentItem) => contentItem.replace(toRemove, "")).filter((contentItem) => contentItem === entryName),
         ),
       )
