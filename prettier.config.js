c8c21e6d60ac93eab54fd7cc6ef5f5c6b62568ac
Lucas Mendonca <mendonca.dev@gmail.com>
2024-02-11 22:55:02 -0300

    Change Prettier config file to prettier.config.js
    This change enables us to use JSDoc comments to type-check the Prettier
    options available.
    
    Also, set endOfLine to "crlf" because it was causing issues when running
    "npm run format" on Linux machine

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+// https://prettier.io/docs/en/configuration
+
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  arrowParens: "always",
+  printWidth: 120,
+  trailingComma: "all",
+  endOfLine: "crlf",
+};
+
+module.exports = config;
